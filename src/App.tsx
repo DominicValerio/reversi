fdcd6370eea97dd13f4343f822f1b9c6a1e23b05
Dominic Valerio <devalerio@outlook.com>
2022-06-18 15:30:00 -0400

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Color, Board} from './board';
+
+class Piece extends React.Component<any> {
+  constructor(props: any) {
+    super(props)
+  }
+
+  getColor(): string {
+    switch (this.props.color) {
+      case Color.Black: return "black"
+      case Color.White: return "white"
+      default: return "none"
+    }
+  }
+
+  render() {
+    return (
+      <>
+      <div id="square"><button className="piece" id={this.getColor()}></button></div>
+      </>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Piece color={Color.White}/><Piece color={Color.White}/>
     </div>
   );
 }
